b84f2c3673400342dbdd7bc0549b601e5f6988ac
Muhammad Danyal Khan <mdk11@ic.ac.uk>
2020-05-23 12:38:27 +0100

    Added RoadAssignment.py based on ping_classification.py from Royal Mail. Util.py also added with utility functions from Royal Mail

---
+++ src/util.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+def haversine_np(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+
+    All args must be of equal length.
+
+    """
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = np.sin(dlat/2.0)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2.0)**2
+
+    c = 2 * np.arcsin(np.sqrt(a))
+    km = 6367 * c
+
+    return km
+
+def add_entity_type(row):
+    if 'M' in row['road']:
+        my_type = "motorway"
+    else:
+        my_type = "A-road"
+
+    return my_type
+
+def sort_list(my_list):
+    return sorted(my_list, key=alpha_num_order)
