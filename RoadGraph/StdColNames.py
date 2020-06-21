480f91884ead7ac804a8d352ab3efa2e187072eb
Muhammad Danyal Khan <mdk11@ic.ac.uk>
2020-06-21 12:10:57 +0100

    Created new python package called RoadGraph, with more well-designed graph builder for the UK road network

---
+++ RoadGraph/StdColNames.py
@@ -0,0 +1,9 @@
+"""
+Equivalent to an enum file, containing the column names of the standard geodataframe used to build the road graph
+"""
+
+STD_ROAD_NO = "ROAD_NO"
+STD_ROAD_TYPE = "ROAD_TYPE"
+STD_LENGTH = "Length"
+STD_IS_SRN = "IS_SRN"
+STD_IS_DIREC = "IS_DIREC"
