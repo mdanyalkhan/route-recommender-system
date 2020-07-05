13a9c35568976c75fe562e957b55386655d25326
Muhammad Danyal Khan <mdk11@ic.ac.uk>
2020-07-05 16:04:51 +0100

    Set up VulnerabilityAnalyser.py and test_VulnerabilityAnalyser.py. Inserted and tested functions generate_square_grid, filter_grid_by_nodes, filter_key_sites, filter_grid_by_key_sites, _nearest_node, and assign_key_sites_to_nearest_nodes

--- RoadGraph/__init__.py
+++ RoadGraph/__init__.py
@@ -4,3 +4,4 @@ from RoadGraph.StdNodesEdgesGdfConnector import *
 from RoadGraph.StdRoadGraphBuilder import *
 from RoadGraph.StdRoadGraph import *
 from RoadGraph.VulnerabilityAnalysis import *
+from RoadGraph.VulnerabilityAnalyser import *
